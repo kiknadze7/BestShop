f564fdf99cc922e23411fe76f5f7a4ac4bae0e59
kikna <mister.kiknadze@gmail.com>
2024-07-07 22:04:03 +0400

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <!-- Meta tags, title, etc. -->
+
+    <!-- Fonts -->
+    <!-- CSS imports -->
+
+    <!-- Custom Styles -->
+    <style type="text/css">
+        .bt-icons i {
+            font-size: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <main class="container">
+        <i class="bi bi-airplane-fill"></i>
+        <i class="bi bi-apple"></i>
+        <i class="bi bi-balloon-heart"></i>
+        <div class="bt-icons">
+            <i class="bi bi-airplane-fill"></i>
+            <i class="bi bi-apple"></i>
+            <i class="bi bi-balloon-heart"></i>
+            ae
+        </div>
+    </main>
+</body>
+
+</html>
